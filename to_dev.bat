df214acbb9967f48ca17cfe9a5e0ff8369b3349a
Hoe Kit <hoekit@gmail.com>
2013-07-26 15:22:07 +0700

    Added files to easily upload changes to firexis.com

---
+++ to_dev.bat
@@ -0,0 +1,1 @@
+ncftpput -f firexis.ftp "/wwwroot/_experiments" "web-export\index.html" "web-export\Mandelbrot_Surfer.pde"
---
+++ to_prod.bat
@@ -0,0 +1,1 @@
+ncftpput -f firexis.ftp "/wwwroot/Mandelbrot-Surfer" "web-export\index.html" "web-export\Mandelbrot_Surfer.pde"
